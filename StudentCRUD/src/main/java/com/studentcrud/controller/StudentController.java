749ef744518b137338976c8c3f702c5d46b8e9a1
surajkatkar <surajkatkar312@gmail.com>
2021-07-21 11:34:53 +0530

    1. added handler for student edit, save , delete
    2. added all required services
    
    3. added new html file for adding new student data

--- StudentCRUD/src/main/java/com/studentcrud/controller/StudentController.java
+++ StudentCRUD/src/main/java/com/studentcrud/controller/StudentController.java
@@ -1,16 +1,60 @@
 package com.studentcrud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.studentcrud.domain.Student;
+import com.studentcrud.services.StudentService;
 
 @Controller
 public class StudentController {
-
+	@Autowired
+	private StudentService service;
+	
 	@GetMapping("/")
-	public String sutdentHandler()
+	public String studentHandler(Model model)
 	{
-		
+		List <Student> listStudent=service.listAll();
+		model.addAttribute(listStudent);
 		return "home";
 	}
 	
+	@GetMapping("/new")
+	public String newAdd(Model model)
+	{
+		model.addAttribute("student", new Student());
+		return "new";
+	}
+	
+	@RequestMapping(value="/save", method = RequestMethod.POST)
+	public String saveStudent(@ModelAttribute ("student") Student std)
+	{
+		service.save(std);
+		return "redirect:/";
+	}
+	
+	@RequestMapping ("/edit/{id}")
+	public ModelAndView editStudentPage(@PathVariable(name="id")int id)
+	{
+		ModelAndView mav=new ModelAndView("new");
+		Student std=service.get(id);
+		mav.addObject("student",std);
+		return mav;
+	}
+	
+	@RequestMapping
+	public String deleteStudent(@PathVariable(name="id")int id)
+	{
+		service.delete(id);
+		return "new";
+	}
 }
--- StudentCRUD/src/main/java/com/studentcrud/services/StudentService.java
+++ StudentCRUD/src/main/java/com/studentcrud/services/StudentService.java
@@ -1,5 +1,9 @@
 package com.studentcrud.services;
 
+import com.studentcrud.domain.Student;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +14,24 @@ import com.studentcrud.repository.StudentRepository;
 public class StudentService {
 	@Autowired
 	private StudentRepository repo;
+	
+	public List <Student> listAll()
+	{
+		return repo.findAll();
+	}
+	
+	public void save(Student std)
+	{
+		repo.save(std);
+	}
+	
+	public Student get(long id)
+	{
+		return repo.findById(id).get();
+	}
+	
+	public void delete(long id)
+	{
+		repo.existsById(id);
+	}
 }
